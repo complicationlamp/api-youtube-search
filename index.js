51ddfb92fc03d421370226411b280612b849e776
Kyle Looker <lookerkj@protonmail.com>
2018-03-10 14:59:07 -0800

    sudocode for the JS

---
+++ index.js
@@ -0,0 +1,29 @@
+const youtubeApiEndpoint = "https://www.googleapis.com/youtube/v3/search"
+
+// sudo code to help guide me along//
+
+//function getDataFromApi(searchthing, callback)
+	//make the query a const object so that we can pass it into json
+	// jQuery $.getJSON(the endpoint, query object, and callback)
+
+
+//function render results ();
+	//return
+	//html that we're going to overlay on the html
+	//anchor taggs for hyperlinks of results
+	//
+
+//function display the youtube search data(data) {
+	//const for the results, pass results into an array
+	// now a js tag so that it'll populate the div}
+
+//function handel the submit button () {
+	// set up a listner .submit (event => {
+		//prevent default
+		//const for querry target 
+		//const make it the value of whateveer we found
+		//call the first function and pass in our new const//
+		//and pass in the dispay function as an argument
+//	})}
+
+//call to listen
